54c3849659e5e5c447bedf78abbf93154aa7e2f9
Ben Bravo <bendc312@gmail.com>
2024-11-11 21:46:56 -0500

    tests good

--- bentiktok/src/main/java/mc/tiktok/MyListener.java
+++ bentiktok/src/main/java/mc/tiktok/MyListener.java
@@ -1,9 +1,12 @@
 package mc.tiktok;
 
+import org.bukkit.Material;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
@@ -16,11 +19,23 @@ public class MyListener implements Listener{
         Player p = e.getPlayer();
         switch (e.getMessage()) {
             case "blind":
-                p.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 50, 255));
+                p.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 75, 255));
             break;
 
             case "drop":
-                
+                ItemStack i = p.getInventory().getItemInMainHand();
+                if (i!=null && i.getAmount()>0) {
+                    p.getWorld().dropItemNaturally(p.getLocation(),i.clone()).setPickupDelay(40);;
+                    p.getInventory().setItemInMainHand(null);
+                }
+            break;
+
+            case "zombie":
+                p.getWorld().spawnEntity(p.getLocation(), EntityType.ZOMBIE);
+            break;
+
+            case "food":
+                p.getInventory().addItem(new ItemStack(Material.COOKED_BEEF, 1));
             break;
 
             default:
